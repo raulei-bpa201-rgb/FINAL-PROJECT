b6d6414836b03d5064e33ced48dc99aa9ecd1026
Your Name <you@example.com>
2026-02-11 20:11:52 +0400

    folders structure migrations and db

---
+++ CareerBridge/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JobPortal.Api.Entities
+{
+    public class User : IdentityUser
+    {
+        public string FullName { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
